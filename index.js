47f178cd4fe9e7603b1239ab217da78c31f6c68c
RuslanPol <vvrv@yandex.ru>
2024-11-07 20:54:11 +0700

    add gitignore

--- index.js
+++ index.js
@@ -1,1 +1,119 @@
-console.log('Happy developing ✨')
+
+/// Первое задание
+console.log("Первое задание-----------------------");
+
+function countPC(number) {
+    let result = "";
+    if (number === 1 || (number % 10 === 1 && number > 20)) {
+        result = `${number} компьютер`;
+    } else if ((1 < number && number < 5) || ((number % 10 > 1 && number % 10 < 5) && number > 20)) {
+        result = `${number} компьютера`;
+    } else {
+        result = `${number} компьютеров`;
+    }
+    return result;
+}
+
+let count = countPC(10);
+console.log(count);
+
+// Второе задание
+console.log("Второе задание-----------------------");
+
+function createArray(array) {
+    let result = [];
+    let num = minNumber(array);
+    while (num >= 1) {
+        for (let i = 0; i < array.length; i++) {
+            if (array[i] % num === 0 && i === array.length - 1) {
+                result.push(num);
+            }
+        }
+        num--;
+    }
+    return result;
+}
+
+function minNumber(nums) {
+    let min = nums[0];
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[i] < min) {
+            min = nums[i];
+        }
+    }
+    return min;
+}
+
+function showArray(array) {
+    array.forEach(i => console.log(i));
+}
+
+let numbers = [8, 12, 4, 16];
+let result = createArray(numbers);
+showArray(result);
+
+// Третье задание
+console.log("Третье задание-----------------------");
+
+function createList(a, b) {
+    let result = [];
+    let tempList = [];
+    if (a < b) {
+        for (let i = a; i <= b; i++) {
+            tempList.push(i);
+        }
+    } else {
+        for (let i = b; i <= a; i++) {
+            tempList.push(i);
+        }
+    }
+
+    tempList.forEach(t => {
+        for (let i = 2; i < t; i++) {
+            if (t % i === 0) {
+                break;
+            }
+            if (i === t - 1) {
+                result.push(t);
+                break;
+            }
+        }
+    });
+
+    return result;
+}
+
+function showList(list) {
+    list.forEach(v => console.log(v));
+}
+
+let list = createList(10, 30);
+showList(list);
+
+// Четвертое задание
+console.log("Четвертое задание-----------------------");
+
+function multiplicationTable(a) {
+    for (let i = 0; i <= a; i++) {
+        if (i < 1) {
+            process.stdout.write("   ");
+        } else {
+            process.stdout.write(`${i}  `);
+        }
+    }
+
+    console.log(" ");
+    for (let i = 1; i <= a; i++) {
+        console.log();
+        process.stdout.write(`${i}`);
+        for (let j = 1; j <= a; j++) {
+            if (i * j / 10 < 1) {
+                process.stdout.write(`  ${i * j}`);
+            } else {
+                process.stdout.write(` ${i * j}`);
+            }
+        }
+    }
+}
+
+multiplicationTable(8);
